d89b4aec78d600e0c8bad37fe14f5a90d6a830bd
rune\81804 <22ci0436@jec.ac.jp>
2023-08-19 03:51:07 +0900

    UI整備 ゲーム画面のUIを整備しました。 ゲームスタート3秒後にPauseボタン表示 Pauseボタンを押したらSetting Tutorial Resume Quitボタン表示 Settingボタンを押したら水色のsettingパネル表示 Backボタン追加（Setting表示からPause表示に戻る機能） などの機能を追加しました

--- bomb/Assets/Resource/Scripts/GameSceneScript.cs
+++ bomb/Assets/Resource/Scripts/GameSceneScript.cs
@@ -7,7 +7,7 @@ using UnityEngine.Events;
 public class GameSceneScript : MonoBehaviour
 {
     GameManager gameManager = GameManager.instance;
-
+  
     public Transform parent; //生成場所指定
     public GameObject wall;//障害壁ランダム格納変数
 
@@ -16,12 +16,16 @@ public class GameSceneScript : MonoBehaviour
     [SerializeField] private GameObject pausePanel;
     //設定ボタンを押した際に発現するパネル
     [SerializeField] private GameObject settingPanel;
+    //チュートリアル（アイテム等説明）ボタンを押した際に発現するパネル
+    [SerializeField] private GameObject tutorialPanel;
+    //音量調整（Sound）ボタンを押した際に発現するパネル
+    [SerializeField] private GameObject soundPanel;
 
 
     //ボタン
-    //ゲーム一時停止ボタン
+    //ゲームを一時停止するボタン
     [SerializeField] private Button pauseButton;
-    //ゲーム再開するボタン
+    //ゲームを再開するボタン
     [SerializeField] private Button resumeButton;
     //ゲーム終了ボタン
     [SerializeField] private Button quitButton;
@@ -31,8 +35,10 @@ public class GameSceneScript : MonoBehaviour
     [SerializeField] private Button soundButton;
     //設定→振動調節ボタン
     [SerializeField] private Button vibrationButton;
-    //チュートリアル表示ボタン
+    //チュートリアル（アイテム等説明）表示ボタン
     [SerializeField] private Button tutorialButton;
+    //Pause画面に戻るボタン
+    [SerializeField] private Button backButton;
 
 
     private void Start()
@@ -42,83 +48,126 @@ public class GameSceneScript : MonoBehaviour
         wall = gameManager.ObstacleWallPrefab[random];
         Instantiate(wall, parent);
 
-        //パネル
+        //パネル（すべて非表示）
         pausePanel.SetActive(false);
         settingPanel.SetActive(false);
-        //ボタン
-        pauseButton.gameObject.SetActive(true);
+        tutorialPanel.SetActive(false);
+        soundPanel.SetActive(false);
 
+        //ボタン
+        //TRUE（表示）
+        pauseButton.gameObject.SetActive(false);
+        Invoke("ShowButtonWithDelay", 3.0f);//ゲームスタートから３秒後にPauseボタンを表示
+
+        //FALSE（非表示）
         resumeButton.gameObject.SetActive(false);
         quitButton.gameObject.SetActive(false);
         settingButton.gameObject.SetActive(false);
         vibrationButton.gameObject.SetActive(false);
         soundButton.gameObject.SetActive(false);
         tutorialButton.gameObject.SetActive(false);
+        backButton.gameObject.SetActive(false);
     }
 
     private void Update()
     {
         //一時停止
-        if (Input.GetKey(KeyCode.Q))
+        if (pauseButton.gameObject.activeSelf && Input.GetKey(KeyCode.Q))
         {
-            PauseGame();
+            PauseButtonDown();
         }
 
         //ゲーム再開
         if (resumeButton.gameObject.activeSelf && Input.GetKey(KeyCode.R))
         {
-            ResumeGame();
+            ResumeButtonDown();
+        }
+    }
+    
+    private void ShowButtonWithDelay()
+    {
+        pauseButton.gameObject.SetActive(true);
+    }
+    public void PauseButtonDown()//一時停止
+    {
+       // if (canPlayPause == true)
+        {
+            Time.timeScale = 0f;
+
+            pauseButton.gameObject.SetActive(false);
+            pausePanel.SetActive(true);
+
+            resumeButton.gameObject.SetActive(true);
+            quitButton.gameObject.SetActive(true);
+            settingButton.gameObject.SetActive(true);
+            tutorialButton.gameObject.SetActive(true);
         }
     }
 
-    public void PauseGame()//一時停止
-    {
-        Time.timeScale = 0f;
-
-        pauseButton.gameObject.SetActive(false);
-        pausePanel.SetActive(true);
-
-        resumeButton.gameObject.SetActive(true);
-        quitButton.gameObject.SetActive(true);
-        soundButton.gameObject.SetActive(true);
-        tutorialButton.gameObject.SetActive(true);
-    }
-
-    public void ResumeGame()//ゲーム再開
+    public void ResumeButtonDown()//ゲーム再開
     {
         pauseButton.gameObject.SetActive(true);
         pausePanel.SetActive(false);
 
         resumeButton.gameObject.SetActive(false);
         quitButton.gameObject.SetActive(false);
-        soundButton.gameObject.SetActive(false);
+        settingButton.gameObject.SetActive(false);
         tutorialButton.gameObject.SetActive(false);
         Time.timeScale = 1f;
     }
 
-    public void QuitGame()//ゲーム終了
+    public void QuitButtonDown()//ゲーム終了
     {
         gameManager.AppQuit();
     }
 
 
-    public void GameSetting()
+    public void SettingButtonDown()
     {
         //パネル
         pausePanel.SetActive(false);
         settingPanel.SetActive(true);
 
         //ボタン
-        pauseButton.gameObject.SetActive(true);
+        // pauseButton.gameObject.SetActive(true);
+        backButton.gameObject.SetActive(true);
+        vibrationButton.gameObject.SetActive(true);
+        soundButton.gameObject.SetActive(true);
+
         resumeButton.gameObject.SetActive(false);
         quitButton.gameObject.SetActive(false);
         settingButton.gameObject.SetActive(true);
-        vibrationButton.gameObject.SetActive(false);
-        soundButton.gameObject.SetActive(false);
+        // vibrationButton.gameObject.SetActive(false);
+        settingButton.gameObject.SetActive(false);
         tutorialButton.gameObject.SetActive(false);
     }
 
-    public void Tutorial()
+    public void TutorialButtonDown()
+    {
+
+    }
+
+    public void BackButtonDown()
+    {
+        pausePanel.SetActive(true);
+        settingPanel.SetActive(false);
+
+        backButton.gameObject.SetActive(false);
+        vibrationButton.gameObject.SetActive(false);
+        soundButton.gameObject.SetActive(false);
+
+        resumeButton.gameObject.SetActive(true);
+        quitButton.gameObject.SetActive(true);
+        settingButton.gameObject.SetActive(true);
+        tutorialButton.gameObject.SetActive(true);
+    }
+
+    public void SoundButtonDown()
+    {
+
+    }
+
+    public void VibrationButtonDown()
     {
 
     }
