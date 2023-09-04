7e56662b9b6d974f6376f55e7880a575e616c7ff
rune\81804 <22ci0436@jec.ac.jp>
2023-09-05 03:12:04 +0900

    BOMB修正 bombPrefabの入れ替えなどbombに関する細かい修正をしました。

--- bomb/Assets/Resources/Scripts/BombScript.cs
+++ bomb/Assets/Resources/Scripts/BombScript.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class BombScript : MonoBehaviour
 {
     public GameObject BombPtrefab;
+    ParticleScript particleScript;
 
+    private void Start()
+    {
+        particleScript = FindObjectOfType<ParticleScript>();
+    }
     public void CreateBomb(Transform player)
     {
         Vector3 newPosition = player.position + new Vector3(0.0f, 1.0f, 0.0f);
         Instantiate(BombPtrefab, newPosition, Quaternion.identity);
+      //  particleScript.Particle();
     }
 }
